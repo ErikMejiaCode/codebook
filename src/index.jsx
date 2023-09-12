adc78e91e24c0cc3a972cbe2734a50c9786790ab
Erik Mejia <erikdojo352@gmail.com>
2023-09-12 09:02:29 -0400

    Scroll to top functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ScrollToTop } from "./components";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>
